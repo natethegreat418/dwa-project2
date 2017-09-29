2ae3495fb17e480c3d4f8292dd775d669cebf02f
Nathan Marcus <nathanimarcus@gmail.com>
2017-09-29 15:16:37 -0400

    calculate class is more narrow in focus, cleaned up variables

--- calculator.php
+++ calculator.php
@@ -4,48 +4,16 @@ namespace TipCalculator;
 
 class TipCalculate {
 
-  public $alert;
-  public $results;
-  public $peoplepaying;
   public $split;
-  public $totalcost;
-  public $tipamt;
-  public $round;
 
-  public function __construct() {
-    $this->peoplepaying = $_GET['splitnum'];
-    $this->totalcost = $_GET['totalcost'];
-    if (isset($_GET['round'])){
-      $this->round = $_GET['round'];
-    }
-    if (isset($_GET['tipamt'])){
-      $this->tipamt = $_GET['tipamt'];
-    }
-    else {
-      $this->tipamt = 0;
-    }
-    if (empty($this->totalcost) === true and empty($this->split) === true) {
-        $this->alert = 'alert-danger';
-        $this->results = 'Something went wrong';
-        return $this->alert;
-        return $this->results;
-      }
+  public function splitRounded($totalcost, $tipamt, $peoplepaying) {
+    $this->split = round($totalcost * (1 + $tipamt) / $peoplepaying);
+    return $this->split;
   }
 
-  public function splitRounded() {
-    $this->split = round($this->totalcost * (1 + $this->tipamt) / $this->peoplepaying);
-    $this->alert = 'alert-info';
-    $this->results = 'Each individual owes '.$this->split.' dollars.';
-    return $this->alert;
-    return $this->results;
-  }
-
-  public function split() {
-    $this->split = $this->totalcost * (1 + $this->tipamt) / $this->peoplepaying;
-    $this->alert = 'alert-info';
-    $this->results = 'Each individual owes '.$this->split.' dollars.';
-    return $this->alert;
-    return $this->results;
+  public function split($totalcost, $tipamt, $peoplepaying) {
+    $this->split = $totalcost * (1 + $tipamt) / $peoplepaying;
+    return $this->split;
   }
 
 }
--- getformfetch.php
+++ getformfetch.php
@@ -4,17 +4,47 @@ require('calculator.php');
 
 use TipCalculator\TipCalculate;
 
+# Check for form submit
 if (isset($_GET['submit'])){
+  # Set variables
   $calculate = new TipCalculate();
-  if ($calculate->round === 'yes'){
-      $calculate->splitRounded();
-      $alert = $calculate->alert;
-      $results = $calculate->results;
-    }
+  $peoplepaying = $_GET['splitnum'];
+  $totalcost = $_GET['totalcost'];
 
-    else {
-      $calculate->split();
-      $alert = $calculate->alert;
-      $results = $calculate->results;
+  if (isset($_GET['round'])){
+    $round = $_GET['round'];
+  }
+
+  else {
+    $round = 0;
+  }
+
+  if (isset($_GET['tipamt'])){
+    $tipamt = $_GET['tipamt'];
+  }
+
+  else {
+    $tipamt = 0;
+  }
+  # Error out if key variables are empty
+  if (empty($totalcost) === true and empty($split) === true) {
+    $alert = 'alert-danger';
+    $results = 'Something went wrong';
+  }
+  
+  # Submit is good: Perform math via Calculate class
+  else {
+    # Round return path
+    if ($round === 'yes'){
+      $calculate->splitRounded($totalcost, $tipamt, $peoplepaying);
+      $alert = 'alert-info';
+      $results = 'Each individual owes '.$calculate->split.' dollars.';
+    }
+    # Un-rounded return path
+    else if ($round === 0){
+      $calculate->split($totalcost, $tipamt, $peoplepaying);
+      $alert = 'alert-info';
+      $results = 'Each individual owes '.$calculate->split.' dollars.';
     }
   }
+}
